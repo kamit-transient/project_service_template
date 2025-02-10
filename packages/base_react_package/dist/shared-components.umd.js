3a272c62ea3edeec96908016ecc195012ef7fd89
bapujiom <hey.amit@outlook.com>
2025-02-10 23:16:35 +0530

    feat: add package for react component

--- packages/base_react_package/dist/shared-components.umd.js
+++ packages/base_react_package/dist/shared-components.umd.js
@@ -1,4 +1,4 @@
-(function(h,T){typeof exports=="object"&&typeof module<"u"?T(exports,require("react")):typeof define=="function"&&define.amd?define(["exports","react"],T):(h=typeof globalThis<"u"?globalThis:h||self,T(h["@kamit-transient/base_react_package"]={},h.React))})(this,function(h,T){"use strict";var W={exports:{}},x={};/**
+(function(h,T){typeof exports=="object"&&typeof module<"u"?T(exports,require("react")):typeof define=="function"&&define.amd?define(["exports","react"],T):(h=typeof globalThis<"u"?globalThis:h||self,T(h.base_react_package={},h.React))})(this,function(h,T){"use strict";var W={exports:{}},x={};/**
  * @license React
  * react-jsx-runtime.production.min.js
  *
--- packages/base_react_package/vite.config.ts
+++ packages/base_react_package/vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import dts from 'vite-plugin-dts';
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        dts({ // Generates .d.ts files
+            insertTypesEntry: true,
+            rollupTypes: true,
+        }),
+    ],
     build: {
         lib: {
             entry: './src/index.ts',
-            name: '@kamit-transient/base_react_package',
+            name: 'base_react_package',
+            fileName: (format) => `shared-components.${format}.js`, // Outputs ESM and UMD
+            formats: ['es', 'umd']
         },
         rollupOptions: {
             external: ['react', 'react-dom'],
