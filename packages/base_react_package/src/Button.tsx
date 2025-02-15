f172c1c784651caf00eb597e3fd2df2a53949a86
bapujiom <hey.amit@outlook.com>
2025-02-15 09:28:12 +0530

    feat: base_react_component is fully functional and generates types, css and every other thing with latest version of vite

--- packages/base_react_package/src/Button.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Button = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <button className="bg-blue-500 text-white px-4 py-2 rounded">
-            {children}
-        </button>
-    );
-};
