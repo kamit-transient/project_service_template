d1f62578dad8b70cf9aeb2c66d744992664d5f56
bapujiom <hey.amit@outlook.com>
2025-02-15 09:26:43 +0530

    feat: base_react_component is fully functional and generates types, css and every other thing with latest version of vite

---
+++ packages/base_react_components/src/index.ts
@@ -0,0 +1,2 @@
+import "./styles.css"
+export { Card } from "./components/cards"
---
+++ packages/base_react_components/vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import dts from 'vite-plugin-dts';
+
+import { peerDependencies } from "./package.json";
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({ tsconfigPath: './tsconfig.app.json', rollupTypes: true, exclude: ["src/**/*.stories.*"] }), // Output .d.ts files
+
+
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'), //'./src/index.ts',
+      name: 'react_components',
+      // fileName: (format) => `react_components.${format}.js`,
+      fileName: 'react_components',
+      formats: ['es', 'cjs', 'umd'],
+      cssFileName: 'styles',
+    },
+    rollupOptions: {
+      external: Object.keys(peerDependencies),
+      output: { globals: { 'react': 'React', 'react-dom': 'ReactDOM' } }
+    }
+  },
+})
